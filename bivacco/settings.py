a2e8c6a76f2de979c7505a79b30cb1c43908f43c
Carlotta Tagliaro <carlotta.tagliaro@gmail.com>
2018-11-16 19:31:27 +0100

    db edited

--- bivacco/settings.py
+++ bivacco/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'locateBivacchi.apps.LocatebivacchiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- locateBivacchi/models.py
+++ locateBivacchi/models.py
@@ -2,24 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class bivacco(models.Model):
+class Bivacco(models.Model):
     coordinate_x = models.FloatField()
     coordinaye_y = models.FloatField()
     capability = models.IntegerField(default=0)
     height = models.IntegerField()
 
-class utente(models.Model):
-    name = models.TextField(max_length=30)
-    surname = models.TextField(max_length=30)
-    CF = models.TextField(max_length=16 primary_key=True)
-    email = models.EmailField
-
-class reservation(models.Model):
-    id_bivacco = models.ForeignKey(bivacco, on_delete=models.CASCADE)
+class Reservation(models.Model):
+    id_bivacco = models.ForeignKey(Bivacco, on_delete=models.CASCADE)
     start_date = models.DateField()
-    end_date = model.DateField()
-    person_number = model.IntegerField(default=0)
-    id_utente = models.ForeignKey(utente, on_delete=models.CASCADE)
+    end_date = models.DateField()
+    code = models.IntegerField(default=1234)
+    person_number = models.IntegerField(default=0)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
