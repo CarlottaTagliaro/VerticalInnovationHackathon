296658a6403ef3c05fe0ed1100fbd14dbb70bf67
abombardelli <abombardelli@fbk.eu>
2018-11-16 22:19:10 +0100

    add login, registration is still broken

--- locateBivacchi/urls.py
+++ locateBivacchi/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url('signup', views.userSignup, name='signup'),
+    url('login', LoginView.as_view(template_name='locateBivacchi/login.html'), name='login'),
+    url('', views.index, name='index'),
 ]
--- locateBivacchi/views.py
+++ locateBivacchi/views.py
@@ -6,7 +6,24 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def userSignup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home') # TODO: cambiare questo
+    else:
+        form = UserCreationForm()
+    return render(request, 'locateBivacchi/signup.html', {'form': form})
