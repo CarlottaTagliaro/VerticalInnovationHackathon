c429a80365d476542c9024922adfb0f7d21f4066
ballerin <francesco@ballerin.it>
2018-11-17 08:32:42 +0100

    fixed login

--- locateBivacchi/urls.py
+++ locateBivacchi/urls.py
@@ -15,16 +15,16 @@ from django.conf.urls import url
 from django.urls import path
 from django.contrib.auth.views import LoginView
 from . import views
-from django.contrib.auth.views import logout
+from django.contrib.auth.views import logout, login
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', login, {'template_name': '/locateBivacchi/login.html'} , name='index'),
     path('temperature/<int:biv_pk>/', views.temperature, name='temperature'),
     path('bar/<int:bar_pk>/', views.bar),
     path('checkcode/<int:biv_pk>/', views.checkCode),
     url(r'^map$', views.map, name='map'),
     url(r'^signup', views.userSignup, name='signup'),
-    url(r'^login$', LoginView.as_view(template_name='locateBivacchi/login.html'), name='login'),
+    url(r'^login$', login, {'template_name': '/locateBivacchi/login.html'}, name='login'),
     url(r'^logout/$', logout ,{'next_page': '/'}),
     path('checkavailable/<int:id_bivacco>/<int:person_number>/<int:day_start>/<int:month_start>/<int:year_start>/<int:day_end>/<int:month_end>/<int:year_end>/', views.checkBivaccoAvailability, name="check_availability"),
     path('reserve/<int:id_bivacco>/<int:person_number>/<int:day_start>/<int:month_start>/<int:year_start>/<int:day_end>/<int:month_end>/<int:year_end>/', views.reserveBivacco, name="reserve"),
