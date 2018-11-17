ea189b34a99fef6551f6b13d5bb0b937fff2ac21
ballerin <francesco@ballerin.it>
2018-11-17 08:14:15 +0100

    Added logout

--- locateBivacchi/urls.py
+++ locateBivacchi/urls.py
@@ -15,6 +15,7 @@ from django.conf.urls import url
 from django.urls import path
 from django.contrib.auth.views import LoginView
 from . import views
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -24,6 +25,7 @@ urlpatterns = [
     url(r'^map$', views.map, name='map'),
     url(r'^signup', views.userSignup, name='signup'),
     url(r'^login$', LoginView.as_view(template_name='locateBivacchi/login.html'), name='login'),
+    url(r'^logout/$', logout ,{'next_page': '/'}),
     path('checkavailable/<int:id_bivacco>/<int:person_number>/<int:day_start>/<int:month_start>/<int:year_start>/<int:day_end>/<int:month_end>/<int:year_end>/', views.checkBivaccoAvailability, name="check_availability"),
     path('reserve/<int:id_bivacco>/<int:person_number>/<int:day_start>/<int:month_start>/<int:year_start>/<int:day_end>/<int:month_end>/<int:year_end>/', views.reserveBivacco, name="reserve"),
     path('reservation', views.reservations),
