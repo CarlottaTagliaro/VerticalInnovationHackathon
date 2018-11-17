066b1d003b4c33f37a0d89e62a5e28f444e66651
Carlotta Tagliaro <carlotta.tagliaro@gmail.com>
2018-11-17 08:40:28 +0100

    map fixed (kinda)

--- locateBivacchi/static/map.js
+++ locateBivacchi/static/map.js
@@ -1,5 +1,7 @@
 var mymap;
 function init() {
+  var h = window.innerHeight;
+  var w = window.innerWidth;
   mymap = L.map('mapid').setView([46.4974, 11.3537], 11);
 
    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -9,6 +11,14 @@ function init() {
      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
    }).addTo(mymap);
+   function resize() {
+     document.getElementById("mapid").style.height = (h-56)+"px";
+     document.getElementById("mapid").style.height = w+"px";
+   }
+   resize();
+   window.onresize = function() {
+     resize();
+   };
    mymap.on("popupopen", function(e) { kkk = e;})
 }
 
